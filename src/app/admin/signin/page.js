329eff7373780c9f39c0f1651ebbd8a3dc4378f7
pranta <you@example.com>
2024-09-10 14:33:14 +0600

    Comment out header section form admin/signin page

--- src/app/admin/signin/page.js
+++ src/app/admin/signin/page.js
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState } from 'react'
 import "./style.scss"
-import Header from "@/shared/components/header/header"
+// import Header from "@/shared/components/header/header"
 import loginImg from '../../../assets/images/signin/signin.jpg'
 import { BACKEND_URL } from '@/shared/constants/ulrList';
 import Cookies from 'js-cookie';
@@ -37,6 +37,8 @@ const Index = () => {
     const handleChange = (e) => {
         const name = e.target.name
         const value = e.target.value
+        console.log("name ==>>", name)
+        console.log("value ==>>", value)
 
         setInput((state) => {
             return {
@@ -48,7 +50,7 @@ const Index = () => {
 
     return (
         <div>
-            <Header />
+            {/* <Header /> */}
             <div className="signin-container">
                 <form className="contact2-form validate-form" autoComplete='false' onSubmit={onSubmit}>
                     <span className="contact2-form-title">
