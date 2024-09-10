a9e37da19f22ac873bceee894ae64c3162976801
pranta <you@example.com>
2024-09-10 09:18:17 +0600

    Change the Backend url

--- src/shared/constants/ulrList.js
+++ src/shared/constants/ulrList.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "http://localhost:5001"
-// export const BACKEND_URL = "https://server.dainikpatrapatrika.com"
+// export const BACKEND_URL = "http://localhost:5001"
+export const BACKEND_URL = "https://server.dainikpatrapatrika.com"
 
