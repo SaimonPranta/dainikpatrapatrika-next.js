276e1c26a750afc6e943840df0a56c77c68a410a
pranta <you@example.com>
2024-09-11 14:47:22 +0600

    Update next.config.mjs file for image optimization

--- src/shared/constants/ulrList.js
+++ src/shared/constants/ulrList.js
@@ -1,5 +1,5 @@
-export const BACKEND_URL = "https://server.dainikpatrapatrika.com"
-// export const BACKEND_URL = process.env.NODE_ENV === "production" ? "https://server.dainikpatrapatrika.com" : "http://localhost:5001"
+// export const BACKEND_URL = "https://server.dainikpatrapatrika.com"
+export const BACKEND_URL = process.env.NODE_ENV === "production" ? "https://server.dainikpatrapatrika.com" : "http://localhost:5001"
 
 
  
