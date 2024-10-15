e87def0fa98fde52baa7227b2e32fbcfcc8bba87
pranta <you@example.com>
2024-10-15 10:38:31 +0600

    Added title sliceing function

--- src/shared/functions/textSlicer.js
+++ src/shared/functions/textSlicer.js
@@ -9,4 +9,32 @@ const textSlicer = (text = "", length = 10, isUseDot) => {
     return slicedText
 };
 
+const slicer = (text, length = 100) => {
+  const textArray = text.split(" ");
+  const replaceElement = [",", "|", "।"]
+
+  let characterCount = 0;
+  let newText = ''
+
+  textArray.forEach((word, index) => {
+    characterCount += word.length
+ 
+    const nextWord = textArray[index + 1] 
+    let lastWord = false
+    if ( nextWord && (characterCount + nextWord.length) > length) {
+      lastWord = true
+    }
+    if (characterCount <= length) {
+      let modifyWord = ` ${word}` 
+      if (lastWord) {
+        let lastCharacter = modifyWord[modifyWord.length - 1]  
+        replaceElement.forEach((ele) => {
+          lastCharacter = lastCharacter.replace(ele, "")
+        }) 
+        modifyWord =   modifyWord.replace(/.$/, lastCharacter)
+      }
+      newText += modifyWord
+    }
+  });
+
 export default textSlicer;
